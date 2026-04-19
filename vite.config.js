e3824f922b7f5c6431f0fdddbe532a1b4f9faa52
pixelgabe <gabrielhperegrino@gmail.com>
2026-04-18 23:55:07 -0700

    Fix: Update base path to relative for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Story-Master/',
+  base: '/storyboarder-master/', // Use '/storyboarder-master/' for github pages, or '/' if using a custom domain.
   plugins: [react()],
 })
